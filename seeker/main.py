d6d1e8acf3d9cac1597331d5f2e5a3e7568c2e57
ecerquei <ecerquei@redhat.com>
2021-08-31 09:28:13 -0400

    2021-08-31 09:28:13.172854 new snippets

--- seeker/main.py
+++ seeker/main.py
@@ -1,10 +1,15 @@
 from provider import Gists
 from seeker.util import git_push, purge
+import logging
 
 if __name__ == '__main__':
     print("Seeker is running ...")
+    logging.info("Seeker is running ...")
     g = Gists()
     # life cycle
+    logging.info("getting new snippets")
     g.get()
+    logging.info("purging old snippets")
     purge()
+    logging.info("pushing to repo")
     git_push()
