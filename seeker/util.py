c14857ceb80517ed44ead6a05091cff8744d5435
ecerquei <ecerquei@redhat.com>
2021-08-30 22:06:23 -0400

    tweaking report

--- seeker/util.py
+++ seeker/util.py
@@ -2,6 +2,7 @@ from configparser import SafeConfigParser
 import json
 from subprocess import call
 from datetime import datetime
+from os import remove, rename
 
 
 def get_config(section, parameter):
@@ -10,21 +11,29 @@ def get_config(section, parameter):
     return json.loads(config.get(section, parameter))
 
 
+def prepend_line(file_name, line):
+    dummy_file = file_name + '.bak'
+    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        write_obj.write(line + '\n')
+        for line in read_obj:
+            write_obj.write(line)
+    remove(file_name)
+    rename(dummy_file, file_name)
+
+
 def git_status(now):
     rs = call("status", shell=True)
     line = "-" * 80
     report_header = f"{line}\n " \
                     f"{now}\n" \
                     f"{line}\n "
-    with open("report.txt", "a") as report:
-        data = f"{report_header} {rs}"
-        report.write(data)
+    prepend_line("report.txt", f"{report_header} {rs}")
 
 
 def git_push():
     now = datetime.now()
     git_status(now)
     commit_message = f"{now} new snippets"
-    call("git add snippet", shell=True)
+    call("git add", shell=True)
     call('git commit -m "' + commit_message + '"', shell=True)
     call('git push origin main', shell=True)
