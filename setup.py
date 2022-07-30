fb392fae0719d6c7e0eeb96c0cd8b37ebf77cfd6
Eduardo Cerqueira <ecerquei@redhat.com>
2022-07-30 15:22:23 -0400

    bumping release to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="seeker",
-    version="0.0.1",
+    version="0.0.2",
     author="Eduardo Cerqueira",
     author_email="eduardomcerqueira@gmail.com",
     description="seeking for code snippet",
