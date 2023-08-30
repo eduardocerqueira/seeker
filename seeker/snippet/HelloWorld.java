be4f069d3d13550fc7c272c3da2b7566fe7f1341
eduardocerqueira <eduardomcerqueira@gmail.com>
2023-08-30 17:09:46 +0000

    2023-08-30 17:09:46.911240 new snippets

---
+++ seeker/snippet/HelloWorld.java
@@ -0,0 +1,9 @@
+//date: 2023-08-30T17:09:05Z
+//url: https://api.github.com/gists/058dfad6982e282b5b6878377df5003f
+//owner: https://api.github.com/users/jaysinha3595
+
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
