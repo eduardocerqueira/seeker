344d01c9a5d69cfda52d7d2393bc512d1733c5dc
eduardocerqueira <eduardomcerqueira@gmail.com>
2023-09-04 17:09:49 +0000

    2023-09-04 17:09:48.965933 new snippets

---
+++ seeker/snippet/send_to_clipboard_xserver.sh
@@ -0,0 +1,5 @@
+#date: 2023-09-04T16:59:10Z
+#url: https://api.github.com/gists/4ffcffa72577cc78a7576e29c1c9b95a
+#owner: https://api.github.com/users/random-tm
+
+echo "text" | xclip -selection clipboard
---
+++ seeker/snippet/setupForBV.sh
@@ -0,0 +1,15 @@
+#date: 2023-09-04T17:08:20Z
+#url: https://api.github.com/gists/aa913f987cb9e6375636a06856aeb0ec
+#owner: https://api.github.com/users/eduanmoldeep
+
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo timedatectl set-timezone Asia/Kolkata
+
+sudo apt-get install -y libgbm-dev
+sudo apt-get install -y libasound2
+
+sudo useradd -m bvbot
