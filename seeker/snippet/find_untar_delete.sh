2363405e8df831cf7042400f789d7d0d90c168d0
eduardocerqueira <eduardomcerqueira@gmail.com>
2023-11-27 17:10:39 +0000

    2023-11-27 17:10:39.610459 new snippets

--- seeker/snippet/find_untar_delete.sh
+++
@@ -1,5 +0,0 @@
-#date: 2023-11-23T16:53:23Z
-#url: https://api.github.com/gists/d925ffbd36442746216a3b0c4de400eb
-#owner: https://api.github.com/users/vadimkantorov
-
-find -name '*.tar.gz' -type f -exec tar -xf {} \; -delete
