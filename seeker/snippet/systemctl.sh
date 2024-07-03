8025d4b2c1d24baa5e49e1e94f41327638f65bcf
eduardocerqueira <eduardomcerqueira@gmail.com>
2024-07-03 17:11:15 +0000

    2024-07-03 17:11:15.022730 new snippets

---
+++ seeker/snippet/systemctl.sh
@@ -0,0 +1,7 @@
+#date: 2024-07-03T17:00:22Z
+#url: https://api.github.com/gists/cfa4985a18465010ebe908ba2d2c1aef
+#owner: https://api.github.com/users/razhangwei
+
+systemctl status nginx
+
+systemctl stop nginx
