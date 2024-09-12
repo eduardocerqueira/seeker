b1e924645c62aa3bdc255688708edb86e3d91157
eduardocerqueira <eduardomcerqueira@gmail.com>
2024-09-12 17:12:36 +0000

    2024-09-12 17:12:36.710489 new snippets

--- seeker/snippet/main.py
+++ seeker/snippet/main.py
@@ -1,12 +1,20 @@
-#date: 2024-09-11T17:10:14Z
-#url: https://api.github.com/gists/dd08b2806979cb4b7228e7565046f675
+#date: 2024-09-12T17:06:45Z
+#url: https://api.github.com/gists/90f6017582cecd5f00eb132d45a73172
 #owner: https://api.github.com/users/mypy-play
 
-def handle(err: tuple[type[ValueError] | type[TypeError], ...]):
-    try:
-        return None
-    except err:
-        pass
+from dataclasses import asdict
+
+import dataclasses
+
+@dataclasses.dataclass
+class Foo:
+    a_string: int = 1
+    a_float: str = "Hey"
+    invalid_param: None = None
 
 
-handle((ValueError, TypeError))
+def bar(a_string: str, a_float: float) -> None:
+    ...
+    
+foo = Foo()
+bar(**asdict(foo))
