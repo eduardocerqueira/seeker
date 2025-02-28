8fa2cac94e213a876a2f2efbf6cea630e9bc7ca0
eduardocerqueira <eduardomcerqueira@gmail.com>
2025-02-28 17:12:38 +0000

    2025-02-28 17:12:38.290635 new snippets

---
+++ seeker/snippet/main.py
@@ -0,0 +1,7 @@
+#date: 2025-02-28T16:46:39Z
+#url: https://api.github.com/gists/e16379d870ea8e5b86b7bbd94e82223c
+#owner: https://api.github.com/users/mypy-play
+
+from collections.abc import Iterable
+
+foo: Iterable[str] = list("bar")
